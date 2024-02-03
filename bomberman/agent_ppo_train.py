3b9b4fddb9ec7daa398f0d50a976c17ddd0240ee
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2024-02-03 01:38:49 -0500

    world generation

--- bomberman/agent_ppo_train.py
+++ bomberman/agent_ppo_train.py
@@ -7,17 +7,17 @@ import matplotlib.pyplot as plt
 
 from components.environment.config import (
     FWD_MODEL_CONNECTION_DELAY,
-    FWD_MODEL_CONNECTION_RETRIES, 
-    FWD_MODEL_URI,  
+    FWD_MODEL_CONNECTION_RETRIES,
+    FWD_MODEL_URI,
 )
 from components.environment.gym import Gym, GymEnv
-from components.environment.mocks import MOCK_15x15_INITIAL_OBSERVATION
+from components.environment.mocks import *
 from components.models.ppo import PPO
 from components.action import make_action
 from components.reward import calculate_reward
 from components.state import (
-    action_dimensions, 
-    state_dimensions, 
+    action_dimensions,
+    state_dimensions,
     observation_to_state
 )
 from components.types import State
@@ -37,10 +37,10 @@ STEPS = 2400
 BATCH_SIZE = 128
 LEARNING_RATE_ACTOR = 0.0003
 LEARNING_RATE_CRITIC = 0.001
-K_EPOCHS = 80 # update policy for K epochs in one PPO update
+K_EPOCHS = 80  # update policy for K epochs in one PPO update
 GAMMA = 0.99
 TAU = 0.005
-EPS_CLIP = 0.2 # clip parameter for PPO
+EPS_CLIP = 0.2  # clip parameter for PPO
 ACTION_STD = 0.6
 HAS_CONTINUOUS_ACTION_SPACE = False
 PRINT_EVERY = 100
@@ -50,11 +50,12 @@ SAVE_EVERY = 10000
 """
 Epsilon-greedy action selection.
 """
-def select_action(agent: PPO, state: State, steps_done: int, verbose: bool = True):
 
+
+def select_action(agent: PPO, state: State, steps_done: int, verbose: bool = True):
     agent_id = AGENTS[steps_done % 2]
     unit_id = UNITS[steps_done % 6]
-    
+
     if verbose:
         print(f"Agent: {agent_id}, Unit: {unit_id}")
 
@@ -85,7 +86,8 @@ async def train(env: GymEnv, agent: PPO):
             else:
                 next_observation, done, info = await env.step([action_or_idle])
 
-            reward = calculate_reward(prev_observation, action, next_observation, current_agent_id=agent_id, current_unit_id=unit_id)
+            reward = calculate_reward(prev_observation, action, next_observation, current_agent_id=agent_id,
+                                      current_unit_id=unit_id)
             next_state = observation_to_state(next_observation, current_agent_id=agent_id, current_unit_id=unit_id)
 
             # saving reward and is_terminals
@@ -111,9 +113,9 @@ async def train(env: GymEnv, agent: PPO):
 
         # Compute statistics
         cumulative_rewards.append(cumulative_reward)
-    
+
     print("Drawing plot: reward distribution over epochs")
-    epochs = range(1, EPOCHS + 1) 
+    epochs = range(1, EPOCHS + 1)
     ax = plt.axes()
     ax.plot(epochs, cumulative_rewards)
     ax.set_title('Cumulative reward by epoch')
@@ -123,66 +125,109 @@ async def train(env: GymEnv, agent: PPO):
     plt.savefig("agent_ppo_rewards.png")
 
 
+def jsonize(json_data):
+    if 'agents' in json_data:
+        return json_data
+    json_data["agents"] = {
+        "a": {
+            "agent_id": "a",
+            "unit_ids": [
+                "c",
+                "e",
+                "g"
+            ]
+        },
+        "b": {
+            "agent_id": "b",
+            "unit_ids": [
+                "d",
+                "f",
+                "h"
+            ]
+        }
+    }
+    json_data['game_id'] = 'dev'
+    json_data['world'] = {
+        "width": 15,
+        "height": 15
+    }
+    json_data['tick'] = 0
+    json_data['config'] = {
+        "tick_rate_hz": 10,
+        "game_duration_ticks": 300,
+        "fire_spawn_interval_ticks": 2
+    }
+    json_data['unit_state'] = json_data.pop('units')
+    unit_states = {d["unit_id"]: d for d in json_data['unit_state']}
+    json_data['unit_state'] = unit_states
+
+    return json_data
+
+
 async def main():
     print("============================================================================================")
     print("PPO agent")
-    print("Connecting to gym")
-    gym = Gym(FWD_MODEL_URI)
-    for retry in range(1, FWD_MODEL_CONNECTION_RETRIES):
-        try:
-            await gym.connect()
-        except:
-            print(f"Retrying to connect with {retry} attempt...")
-            time.sleep(FWD_MODEL_CONNECTION_DELAY)
-            continue
-        break
-    print("Connected to gym successfully")
-    print("============================================================================================")
 
     print("============================================================================================")
     print("Initializing agent")
-    env = gym.make("bomberland-gym", MOCK_15x15_INITIAL_OBSERVATION)
-    observation = await env.reset()
-    n_states = state_dimensions(observation)
-    n_actions = action_dimensions()
-    print(f"Agent: states = {n_states}, actions = {n_actions}")
+    maps = [jsonize(MOCK_15x15_INITIAL_OBSERVATION), jsonize(MOCK_15x15_INITIAL_OBSERVATION_1235),
+            jsonize(MOCK_15x15_INITIAL_OBSERVATION_1236),
+            ]
+    for seed_map in maps:
+        print("Connecting to gym")
+        gym = Gym(FWD_MODEL_URI)
+        for retry in range(1, FWD_MODEL_CONNECTION_RETRIES):
+            try:
+                await gym.connect()
+            except:
+                print(f"Retrying to connect with {retry} attempt...")
+                time.sleep(FWD_MODEL_CONNECTION_DELAY)
+                continue
+            break
+        print("Connected to gym successfully")
+        print("============================================================================================")
+        env = gym.make("bomberland-gym", seed_map)
+        observation = await env.reset()
+        n_states = state_dimensions(observation)
+        n_actions = action_dimensions()
+        print(f"Agent: states = {n_states}, actions = {n_actions}")
 
-    if "agent_ppo.pt" in os.listdir("."):
-        print("Loding pretrained agent ...")
+        if "agent_ppo.pt" in os.listdir("."):
+            print("Loading pretrained agent ...")
 
-    ppo_agent = PPO(
-        n_states, 
-        n_actions, 
-        LEARNING_RATE_ACTOR, 
-        LEARNING_RATE_CRITIC,
-        GAMMA,
-        K_EPOCHS, 
-        EPS_CLIP,
-        HAS_CONTINUOUS_ACTION_SPACE, 
-        ACTION_STD,
-        torch.load("agent_ppo.pt") if "agent_ppo.pt" in os.listdir(".") else None
-    )
+        ppo_agent = PPO(
+            n_states,
+            n_actions,
+            LEARNING_RATE_ACTOR,
+            LEARNING_RATE_CRITIC,
+            GAMMA,
+            K_EPOCHS,
+            EPS_CLIP,
+            HAS_CONTINUOUS_ACTION_SPACE,
+            ACTION_STD,
+            torch.load("agent_ppo.pt") if "agent_ppo.pt" in os.listdir(".") else None
+        )
 
-    print("============================================================================================")
+        print("============================================================================================")
 
-    print("============================================================================================")
-    print("Training agent")
-    start_time = datetime.datetime.now().replace(microsecond=0)
-    print("Started training at (GMT) : ", start_time)
-    await train(env, ppo_agent)
-    end_time = datetime.datetime.now().replace(microsecond=0)
-    print("Started training at (GMT) : ", start_time)
-    print("Finished training at (GMT) : ", end_time)
-    print("Total training time  : ", end_time - start_time)
-    print("============================================================================================")
+        print("============================================================================================")
+        print("Training agent")
+        start_time = datetime.datetime.now().replace(microsecond=0)
+        print("Started training at (GMT) : ", start_time)
+        await train(env, ppo_agent)
+        end_time = datetime.datetime.now().replace(microsecond=0)
+        print("Started training at (GMT) : ", start_time)
+        print("Finished training at (GMT) : ", end_time)
+        print("Total training time  : ", end_time - start_time)
+        print("============================================================================================")
 
-    print("============================================================================================")
-    print("Saving agent")
-    ppo_agent.save()
-    ppo_agent.show()
-    print("============================================================================================")
-    
-    await gym.close()
+        print("============================================================================================")
+        print("Saving agent")
+        ppo_agent.save()
+        ppo_agent.show()
+        print("============================================================================================")
+
+        await gym.close()
 
 
 if __name__ == "__main__":
